19d551f4e83c8ffc83c96be22727dea49750a83c
Sheryl Liang <sheryl@liang.at>
2021-08-07 19:10:11 +0800

    fixed syntax error and updated aapa.py

--- aapa.py
+++ aapa.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[2]:
+# In[1]:
 
 
-pip install munkres
+# install munkres in the terminal
+# pip install munkres
 # https://github.com/bmc/munkres
 
 
-# In[173]:
+# In[4]:
 
 
 ##################################################
@@ -24,6 +25,7 @@ pip install munkres
 import csv
 from munkres import Munkres, print_matrix, make_cost_matrix
 import numpy as np
+from data_processor import DataProcessor
 
 class AAPA:
     # Action-Aware Perceptual Anchoring module
@@ -35,16 +37,6 @@ class AAPA:
         self.max_it = max_iterations
         
 _VERBOSE_            = True
-
-# Constants for reading input data
-_PATH                = '/home/sheryl/Projects/Icarus/src/icarus/'
-_DIGITS              = 5
-_OBJ_SUFFIX          = "obj"
-_RELATION_SUFFIX     = "relations"
-_FILE_TYPE           = ".txt"
-_LABELS              = ['type','id','x','y','z','w','h']
-FIXED_POSITIONS      = []
-OBJ_IDS_TO_EXCLUDE   = ['left','table']
 
 # Constants for camera movement alignment
 HORIZ_OFFSET         = 0.008
@@ -73,23 +65,7 @@ OOV_MIN              = 0
 MIN_INFERENCE_CONF   = 0.1
 
 
-# In[165]:
-
-
-path = '/home/sheryl/Projects/WP1_Human_Activity_Data/S002/image_bbox_S002_1/_generated_aapa/2_1_40_Put_down_the_nut_driver_frames_19674_to_19745/'
-path = '/home/sheryl/Projects/Icarus/src/icarus/'
-frame_no = 1
-# file_path = getFilePath(path, frame_no)
-# readDataFromCsv(file_path)
-# [i for i,element in enumerate([1,2,3])]
-dp = DataProcessor(path)
-obj = dp.preattend(0)
-# labels        = ['type','id','x','y','z','w','h']
-
-# obj
-
-
-# In[181]:
+# In[1]:
 
 
 def print_verbose(output):
@@ -97,59 +73,8 @@ def print_verbose(output):
         print(output)
     return
 
-class DataProcessor:
-    # Reads in data from a csv file and processes it in the required format
-    def __init__(self, path=_PATH):
-        self._PATH = path
 
-    def get_file_path(self, path, frame_no, digits, suffix, file_ending):
-        # returns the file path with a specific file name format
-        # e.g. in this case it is 00000_obj.txt
-        fname = str(frame_no).zfill(digits)
-        if len(suffix) == 0:
-            suffix = ""
-        else:
-            suffix = "_" + suffix
-        file_path = path + fname + suffix + file_ending
-        return file_path
-
-    def read_relations_from_csv(self, cycle):
-        # reads in concept relations
-        # assumes that each line consists of concept name and N variables
-        # e.g. object-on-left,output_subassembly0,case0
-        file_path = self.get_file_path(self._PATH, cycle, _DIGITS, _RELATION_SUFFIX, _FILE_TYPE)
-        lines    = open(file_path, "r")
-        relations_dict = []
-        for line in csv.reader(lines, delimiter=','):
-            relations_dict += [[l for l in line]]
-        return relations_dict
-
-    def read_obj_data_from_csv(self, file_path):
-        # assume that data_path points towards a csv file
-        # where the format is 'objType,objId,x,y,z,w,h'
-        lines    = open(file_path, "r")
-        percepts = []
-        for line in csv.reader(lines, delimiter=','):
-            line = line[:len(_LABELS)]
-            percepts.append([l if i <= 1 else float(l) for i,l in enumerate(line)])
-    #       [objType,objId,x,y,z,w,h] = line
-        return percepts
-
-    def preattend(self, cycle):
-        # Reads in data from CSV as a nested array
-        # E.g. [['hand', 'hand2', '752', '214.5', '0', '124', '147'], ..]
-        # Returns an array of concepts of type with initialised confidence value
-        # [{'type': 'hand', id: 'hand2', 'x':752, ..}]
-
-        file_path     = self.get_file_path(self._PATH, cycle, _DIGITS, _OBJ_SUFFIX, _FILE_TYPE)
-        percepts      = self.read_obj_data_from_csv(file_path)
-        percepts_dict = [dict(zip(_LABELS,p)) for p in percepts]
-        for i in percepts_dict:
-            i.update({'conf': 0})
-        return percepts_dict
-
-
-# In[3]:
+# In[4]:
 
 
 # taken from https://stackoverflow.com/questions/67314897/loop-recursion-to-handle-hierarchical-structures-in-python/67330216?noredirect=1#comment119010808_67330216
@@ -199,7 +124,7 @@ all_passing = {
 print(all_passing)
 
 
-# In[240]:
+# In[5]:
 
 
 def exclude_obj_id(obj_id):
@@ -625,7 +550,7 @@ def reason_on_disappeared_objects(prev_objs, perceived_objs, maintained_objs, re
     #   maintained_objs [dict, dict,..]: current world model
     #   relations: relations betw. objects [('attached',obj1,obj2),...]
     
-    for obj in prev_objs:
+#     for obj in prev_objs:
 #         maintained_objs = anchor_disappeared_obj(obj, perceived_objs, maintained_objs, relations)
 
 #     prev_disjoint = [p for p in prev_objs if p not in get_ids_from_set(maintained_objs)]
@@ -642,7 +567,7 @@ def reason_on_disappeared_objects(prev_objs, perceived_objs, maintained_objs, re
     return maintained_objs
 
 
-# In[248]:
+# In[6]:
 
 
 ##########  FUNCTIONS FOR   #######################
@@ -677,7 +602,7 @@ def reason_on_appeared_objects(assignments, new_state, curr_cycle):
     return new_objs
 
 
-# In[246]:
+# In[7]:
 
 
 
@@ -731,12 +656,6 @@ def publish_pstm(conf_pstm, sim, target_path, cycle, action):
     return
 
 
-# In[ ]:
-
-
-
-
-
 # In[7]:
 
 
@@ -768,6 +687,7 @@ def run_aux(n):
 # In[ ]:
 
 
+# CODE IN CLOJURE
 (def prev-cycle '((hand left *status open *conf 1 *psi -5.7273307997783736E-5 *phi 1.8194561736627286E-4 *y 0.08627867435284144 *theta -1.059244267873855E-4 *agent me *x 0.0707034542503848 *height 0.01 *toolangle 0.5623419496320909 *width 0.01 *z 0.5041641487148527) (table table1 *conf 1 *y 0 *x 0 *height 10 *width 10 *z 0) (plug oil_plug0 *conf 1 *area 0.009928 *y 0.91 *x 0.6135 *height 0.136 *width 0.073 *z 0.218129) (case case1 *conf 1 *area 0.09565799999999999 *y 0.55 *x 0.9445 *height 0.894 *width 0.107 *z 0.347046) (output output_subassembly0 *conf 1 *area 0.025172 *y 0.1015 *x 0.071 *height 0.203 *width 0.124 *z 0.40865) (case case0 *conf 1 *area 0.178362 *y 0.489 *x 0.8765 *height 0.734 *width 0.243 *z 0.490089))
 (def curr-cycle '((hand left *status open *conf 1 *psi 1.6821524339148894E-4 *phi 1.7033738111682756E-4 *y 0.08634850490297065 *theta -5.310337282732053E-4 *agent me *x 0.07056213673888918 *height 0.01 *toolangle 0.5623380849438329 *width 0.01 *z 0.504316614569876) (table table1 *conf 1 *y 0 *x 0 *height 10 *width 10 *z 0) (small_hub_cover oil_plug0 *conf 1 *area 0.00858 *y 0.907 *x 0.619 *height 0.13 *width 0.066 *z 0.211993) (case case1 *conf 1 *area 0.09565799999999999 *y 0.55 *x 0.9445 *height 0.894 *width 0.107 *z 0.347046) (output output_subassembly0 *conf 1 *area 0.025172 *y 0.1015 *x 0.071 *height 0.203 *width 0.124 *z 0.40865) (case case0 *conf 1 *area 0.178362 *y 0.489 *x 0.8765 *height 0.734 *width 0.243 *z 0.490089))
 
@@ -839,264 +759,8 @@ hand,hand1,551.5,235.5,0,119,99
 hand,hand2,727.5,215,0,105,120
 
 
-# In[259]:
-
-
-########### IMPLEMENTATION NOTES ############
-# AAPA was implemented in the following way #
-
-### 1. CAMERA MOVEMENT ALIGNMENT ###
-# This needs to be done first because any positions from the previous cycle might be off using
-# 
-# > newpstm = adjust_poses_from_action(prev_actions[0], newpstm)
-#
-
-### 2. CONSECUTIVE FRAME ALIGNMENT btw newpstm (prev-cycle) and curr-cycle
-# We find the objs that are both in prev and in the new percepts, using
-#
-# > assignments = find_optimal_frame_alignment(newpstm, curr_cycle) 
-#
-# this only accept matches that have <MAX_ALIGNMENT_WEIGHT
-
-### 3. UPDATE NEW STATE FOR VISIBLE OBJECTS
-    # 0. include the currently held object if it has not been aligned
-    # 1. Take prev name and id, update with new position values from curr-cycle
-    # 2. set anchor state to visible, increase confidence
-    # 3. Update children
-        
-    ### 4. Anchor DISAPPEARED objs that have not been updated
-# since we want the unique ids of the prev objs to be maintained we need to do this step first
-# as the ids might clash with the newly detected objs
-    
-    ### 5. Anchor APPEARANCES: Add newly detected objects that have not been assigned
-# add new objs that have not been aligned and assign new ids if they clash with the existing ones
-          
-
-
-################# UNIT TESTS ##############
-# DIFFERENT CASES TO TEST FUNCTIONALITIES #
-###########################################
-
-# prev = ['breather_plug1', 'breather_plug2', 'casing_base1', 'casing_base2', 'output_hub_cover1', 'input_hub_cover1', 'casing_bolt1', 'casing_bolt2', 'casing_nut1', 'input_subassembly1', 'output_subassembly1', 'transfer_subassembly1', 'hand1', 'hand2']
-# curr = ['hand2', 'input_subassembly1', 'casing_bolt2', 'output_subassembly1', 'casing_bolt1', 'casing_base1', 'hand3', 'breather_plug2', 'casing_base2', 'output_hub_cover2', 'transfer_subassembly1', 'casing_nut1', 'breather_plug1', 'small_hub_cover3', 'input_hub_cover1']
-cstm = [['attached', 'input_hub_cover1', 'casing_base1'],['attached', 'casing_bolt2', 'casing_base1'],
-       ['attached', 'breather_plug1', 'casing_base2'],['attached', 'hand1', 'hand2']]
-
-dp = DataProcessor()
-prev = dp.preattend(1)[:]
-curr = dp.preattend(0)[:]
-
-print("------- Running unit tests with the following two sets --------")
-print("PREV: ")
-print([p['id'] for p in prev])
-print(prev)
-print("CURR: ")
-print([p['id'] for p in curr])
-print(curr)
-assert(len(prev) == 14)
-assert(len(curr) == 15)
-print("....... passed")
-
-print("--------------------------------------------------------------")
-print("------- # TEST#2. CONSECUTIVE FRAME ALIGNMENT of both sets --------")
-print("--------------------------------------------------------------")
-al = find_optimal_frame_alignment(prev,curr,1)
-print("Alignments: ")
-print(al)
-assert(al == [('breather_plug1', 'breather_plug2'),
-#  ('breather_plug2', 'breather_plug1'),
- ('casing_base1', 'casing_base1'),
- ('casing_base2', 'casing_base2'),
- ('output_hub_cover1', 'output_hub_cover2'),
- ('casing_bolt1', 'casing_bolt2'),
- ('casing_bolt2', 'casing_bolt1'),
- ('casing_nut1', 'casing_nut1'),
- ('input_subassembly1', 'input_subassembly1'),
- ('output_subassembly1', 'output_subassembly1'),
- ('transfer_subassembly1', 'transfer_subassembly1')])
-print("....... passed")
-
-print("# TEST switched alignments --------")
-switched = get_switched_alignments(al)
-assert(switched == [('breather_plug1', 'breather_plug2'),
-#  ('breather_plug2', 'breather_plug1'),
- ('output_hub_cover1', 'output_hub_cover2'),
- ('casing_bolt1', 'casing_bolt2'),
- ('casing_bolt2', 'casing_bolt1')])
-print("....... passed")
-
-print("# TEST max dissimilarity --------")
-# assert(get_max_dissimilarity(al,prev,curr) == 1.118033988749895) # for alignment threshold 2
-assert(get_max_dissimilarity(al,prev,curr) == 0.5) # for alignment threshold 1
-print("....... passed")
-
-print("# TEST unassigned objects --------")
-unass = get_unassigned_objects(al, prev, curr)
-print("PREV - unassigned:")
-print(unass[0])
-print("CURR - unassigned:")
-print(unass[1])
-assert(unass == [['breather_plug2','input_hub_cover1', 'hand1', 'hand2'],
- ['hand2', 'hand3', 'breather_plug1','small_hub_cover3', 'input_hub_cover1']])
-print("....... passed")
-
-print("--------------------------------------------------------------")
-print("------- # TEST#3. UPDATE NEW STATE for visible assignments ---")
-print("--------------------------------------------------------------")
-new_state = []
-# old_obj = find_obj_by_id('casing_base1', prev)
-# assigned_obj = find_obj_by_id('casing_base2', curr)
-old_obj = {'type': 'casing_base', 'id': 'casing_base1', 'x': 682.0, 'y': 368.5, 'z': 0.0, 'w': 232.0, 'h': 175.0, 'conf': 0.5}
-assigned_obj = {'type': 'casing_base', 'id': 'casing_base2', 'x': 187.5, 'y': 369.5, 'z': 0.0, 'w': 245.0, 'h': 163.0, 'conf': 0}
-
-print("# TEST update new obj with old type, id, conf --------")
-new_obj = update_old_obj_with_assigned(old_obj, assigned_obj)
-assert(new_obj == {'type': 'casing_base', 'id': 'casing_base1', 'x': 187.5, 'y': 369.5, 'z': 0.0, 'w': 245.0, 'h': 163.0, 'conf': 0.5})
-print("....... passed")
-
-print("# TEST update new obj with old type, id, conf --------")
-new_obj['anchor'] = 'visible'
-new_obj = update_confidence(new_obj,0.1)
-# print(new_obj)
-assert(new_obj == {'type': 'casing_base', 'id': 'casing_base1', 'x': 187.5, 'y': 369.5, 'z': 0.0, 'w': 245.0, 'h': 163.0, 'conf': 0.6, 'anchor': 'visible'})
-print("....... passed")
-
-print("# TEST get all children --------")
-parent = 'casing_base1'
-children = get_children(parent,cstm)
-assert(children == ['input_hub_cover1','casing_bolt2'])
-print("....... passed")
-
-print("# TEST get children to update --------")
-children = get_children_to_update(parent,cstm, [i[0] for i in al])
-assert(children == ['input_hub_cover1'])
-print("....... passed")
-
-print("# TEST Create set of visible objs --------")
-# new_state = update_children(new_state, new_obj, old_obj, al, cstm, prev)
-new_state = create_set_of_visible_objects(al, cstm, prev, curr)
-new_state_ids = [p['id'] for p in new_state]
-print("NEW STATE of visible objs:")
-print(new_state_ids)
-assert(new_state_ids == ['breather_plug1', 'casing_base1', 'input_hub_cover1', 
-                         'casing_base2', 'output_hub_cover1', 'casing_bolt1', 'casing_bolt2', 
-                         'casing_nut1', 'input_subassembly1', 'output_subassembly1', 
-                         'transfer_subassembly1'])
-print("....... passed")
-
-
-# TODO
-print("--------------------------------------------------------------")
-print("------- # TEST#4. ANCHOR DISAPPEARANCES from prev state --------")
-print("--------------------------------------------------------------")
-# objs = reason_on_disappeared_objects(prev_actions, prev, curr, new_state, cstm)
-# assert('breather_plug2' in objs)
-print("....... passed")
-
-print("--------------------------------------------------------------")
-print("------- # TEST#5. ANCHOR APPEARANCES from curr state --------")
-print("--------------------------------------------------------------")
-
-print("# TEST assign new symbol --------")
-# new_state = update_children(new_state, new_obj, old_obj, al, cstm, prev)
-unique_id = assign_new_symbol(['case0','case1'], {'id': 'case1', 'type': 'case'})
-assert(unique_id == {'id': 'case2', 'type': 'case'})
-print("....... passed")
-
-print("# TEST reason on appeared objs --------")
-appeared_objs = reason_on_appeared_objects(al, new_state, curr)
-appeared_obj_ids = [p['id'] for p in appeared_objs]
-print("* APPEARED from CURR: ")
-print(appeared_obj_ids)
-assert(appeared_obj_ids == ['hand1', 'hand2', 'breather_plug2', 'small_hub_cover1', 'input_hub_cover2'])
-print("....... passed")
-
-print("# TEST final AAPA output --------")
-new_state += appeared_objs
-print("* FINAL NEW STATE: ")
-new_state_ids = [i['id'] for i in new_state]
-print(new_state_ids)
-assert(new_state_ids == ['breather_plug1', 'casing_base1', 'input_hub_cover1', 'casing_base2', 'output_hub_cover1', 'casing_bolt1', 'casing_bolt2', 'casing_nut1', 'input_subassembly1', 'output_subassembly1', 'transfer_subassembly1', 'hand1', 'hand2', 'breather_plug2', 'small_hub_cover1', 'input_hub_cover2'])
-print("....... passed")
-
-print("ALL PASSED")
-
-# TODO: Cases to check
-# Case1: if obj1 is OOV and occluding another obj2, keep obj2
-
-# Case2: if obj1 is unassigned, but has parent obj2 assigned, keep obj1
-
-# Case3: if obj1 is unassigned, but has parent obj2 unassigned, has parent obj3 which is being dropped,
-# do we still check obj2 for occ/oov and potentially keep it? or do we lose all children obj2 and obj1
-
-# Case4: when checking lost obj1 for occlusion, check if intersects with aligned obj (maintained_objs)
-# as well as newly_perceived_objs
-
-
 # In[ ]:
 
 
-print("--------------------------------------------------------------")
-print("------- # TEST#4. ANCHOR DISAPPEARANCES from prev state --------")
-print("--------------------------------------------------------------")
 
-
-print("# TEST reason on appeared objs --------")
-# INPUT: NEW STATE of visible objs:
-# new_state = ['breather_plug1', 'casing_base1', 'input_hub_cover1', 'casing_base2', 'output_hub_cover1', 'casing_bolt1', 'casing_bolt2', 'casing_nut1', 'input_subassembly1', 'output_subassembly1', 'transfer_subassembly1']
-# cstm = [['attached', 'input_hub_cover1', 'casing_base1'],['attached', 'casing_bolt2', 'casing_base1'],
-#        ['attached', 'breather_plug1', 'casing_base2'],['attached', 'hand1', 'hand2']]
-
-disappeared_objs = reason_on_disappeared_objects(prev, curr, new_state, cstm)
-disappeared_obj_ids = [p['id'] for p in disappeared_objs]
-print("* APPEARED from CURR: ")
-print(disappeared_obj_ids)
-assert(disappeared_obj_ids == ['breather_plug2', 'hand1', 'hand2'])
-print("....... passed")
-
-print("# TEST final AAPA output --------")
-new_state += disappeared_obj
-print("* FINAL NEW STATE: ")
-new_state_ids = [i['id'] for i in new_state]
-print(new_state_ids)
-assert(new_state_ids == ['breather_plug1', 'casing_base1', 'input_hub_cover1', 'casing_base2', 'output_hub_cover1', 'casing_bolt1', 'casing_bolt2', 'casing_nut1', 'input_subassembly1', 'output_subassembly1', 'transfer_subassembly1', 'breather_plug2', 'hand1', 'hand2'])
-print("....... passed")
-
-# TODO: Cases to check
-# Case1: if obj1 is OOV and occluding another obj2, keep obj2
-
-# Case2: if obj1 is unassigned, but has parent obj2 assigned, keep obj1
-
-# Case3: if obj1 is unassigned, but has parent obj2 unassigned, has parent obj3 which is being dropped,
-# do we still check obj2 for occ/oov and potentially keep it? or do we lose all children obj2 and obj1
-
-# Case4: when checking lost obj1 for occlusion, check if intersects with aligned obj (maintained_objs)
-# as well as newly_perceived_objs
-
-
-print("--------------------------------------------------------------")
-print("------- # TEST#5. ANCHOR APPEARANCES from curr state --------")
-print("--------------------------------------------------------------")
-
-print("# TEST assign new symbol --------")
-# new_state = update_children(new_state, new_obj, old_obj, al, cstm, prev)
-unique_id = assign_new_symbol(['case0','case1'], {'id': 'case1', 'type': 'case'})
-assert(unique_id == {'id': 'case2', 'type': 'case'})
-print("....... passed")
-
-print("# TEST reason on appeared objs --------")
-appeared_objs = reason_on_appeared_objects(al, new_state, curr)
-appeared_obj_ids = [p['id'] for p in appeared_objs]
-print("* APPEARED from CURR: ")
-print(appeared_obj_ids)
-assert(appeared_obj_ids == ['hand3', 'hand4', 'breather_plug3', 'small_hub_cover1', 'input_hub_cover2'])
-print("....... passed")
-
-print("# TEST final AAPA output --------")
-new_state += appeared_objs
-print("* FINAL NEW STATE: ")
-new_state_ids = [i['id'] for i in new_state]
-print(new_state_ids)
-assert(new_state_ids == ['breather_plug1', 'casing_base1', 'input_hub_cover1', 'casing_base2', 'output_hub_cover1', 'casing_bolt1', 'casing_bolt2', 'casing_nut1', 'input_subassembly1', 'output_subassembly1', 'transfer_subassembly1', 'breather_plug2', 'hand1', 'hand2', 'hand3', 'hand4', 'breather_plug3', 'small_hub_cover1', 'input_hub_cover2'])
-print("....... passed")
 
